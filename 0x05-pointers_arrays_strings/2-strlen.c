4300bd088095a3c3c33f333c044d34298d4f0256
HamzaMuhd <hamzamuhdidris@gmail.com>
2022-09-19 14:21:25 -0700

    commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,10 +8,10 @@
  **/
 int _strlen(char *s)
 {
-		int len = 0;
+	int len = 0;
 
-		while (*(s + len) != '\0')							     len++;
+	while (*(s + len) != '\0')							     len++;
 
-				return (len);
+	return (len);
 }
 
