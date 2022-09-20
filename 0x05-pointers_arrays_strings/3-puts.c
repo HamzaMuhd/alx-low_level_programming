fda95a87ad8d1a78acee06ac2bde4f3a3f795cb8
hamzamuhd <hamzamuhdidris@gmail.com>
2022-09-20 17:34:35 +0100

    commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- *  * _puts - print a given string to screen
- *   *
- *    * @str: passed pointer argument for string
- *     * Return: void
- **/
+ * _puts - print a given string to screen
+ *
+ * @str: passed pointer argument for string
+ * Return: void
+ */
 void _puts(char *str)
 {
-		int len = 0;
+	int len = 0;
 
-			while (*(str + len) != '\0')
-					{
-								_putchar(*(str + len));
-										len++;
-											}
-				_putchar('\n');
+	while (*(str + len) != '\0')
+	{
+		_putchar(*(str + len));
+		len++;
+	}
+	_putchar('\n');
 }
